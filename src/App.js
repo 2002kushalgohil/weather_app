c66fde1c3c0d5727fb2e68e12a2fa2e7a01dc866
kushal gohil <2002kushalgohil@gmail.com>
2021-09-15 12:52:26 +0530

    finished function in app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
+  const [inputData, setInputData] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const apiKey = `d5bbf8c46db7f8f692314b8e6700b9af`;
+  const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${inputData}&units=metric&APPID=${apiKey}`;
+
+  let date = String(new window.Date());
+  date = date.slice(3, 15);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await fetch(apiLink);
+    const data = await response.json();
+    setLoading(false);
+    setWeatherData(data);
+    if (data.cod === "404") {
+      toast.error("No Such City Found", {
+        position: "top-center",
+      });
+    }
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setInputData("");
+    if (inputData === "") {
+      toast.error("Enter City Name", {
+        position: "top-center",
+      });
+    }
+    fetchData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="HomeDiv">
+        <div id="card">
+          <div id="cardInput">
+            <form onSubmit={submitHandler}>
+              <input
+                type="text"
+                placeholder="City Name.."
+                onChange={(e) => {
+                  setInputData(e.target.value);
+                }}
+                value={inputData}
+              />
+              <button onClick={submitHandler}>Go</button>
+            </form>
+          </div>
+          {typeof weatherData.main === "undefined" ? (
+            <div id="cardData">
+              {loading ? <p>Loading</p> : ""}
+              <p>Search A City</p>
+            </div>
+          ) : (
+            <div id="cardData">
+              <p>City- {weatherData.name}</p>
+              <p>Date- {date}</p>
+              <p>Temprature- {weatherData.main.temp}</p>
+              <p>Weather- {weatherData.weather[0].main}</p>
+            </div>
+          )}
+        </div>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
